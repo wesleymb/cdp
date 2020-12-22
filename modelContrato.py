b5774230d175131f40309e2a02ff9082f9bb8e15
Wesley Menezes <wesleyafdp@gmail.com>
2020-12-22 14:19:45 -0300

    Ajustes na modelContrato.py recebendo uma arrey

--- modelContrato.py
+++ modelContrato.py
@@ -1,36 +1,42 @@
 import datetime
 from dateutil.relativedelta import relativedelta
 
+
 class contrato:
-	def __init__(self, numeroDeProcesso, dataAssinatura, gestor, vencimento, status):
-		self.numeroDeProcesso = numeroDeProcesso
-		self.dataAssinatura = dataAssinatura
-		self.gestor = gestor
-		self.vencimento = vencimento
-		self.status = status
+	def __init__(self,listaDedadosContrato):
+		self.listaDedadosContrato = listaDedadosContrato
+		self.ajustarDados()
+	
+	def ajustarDados(self):
+		self.id = self.listaDedadosContrato[0]
+		self.contrato = self.listaDedadosContrato[1]
+		self.numeroDeProcesso = self.listaDedadosContrato[2]
+		self.objeto = self.listaDedadosContrato[3]
+		self.empresa = self.listaDedadosContrato[4]
+		self.dataAssinatura = self.listaDedadosContrato[5]
+		self.dataTermino = self.listaDedadosContrato[6]
+		self.vencimento = self.listaDedadosContrato[7]
+		self.idServidorGestor = self.listaDedadosContrato[8]
+		self.status = self.listaDedadosContrato[9]
 
 
 	def gerarVencimentos(self):
 
+		vencimento = datetime.datetime.strptime(self.vencimento,"%Y-%m-%d").date()
 		meses = 0		
 		listaDeDias = []
 		while meses <= 12:
-			dataMesQueVem = self.vencimento + relativedelta(months=meses)
+			dataMesQueVem = vencimento + relativedelta(months=meses)
 			listaDeDias.append(dataMesQueVem)
 			meses = meses + 1
 		return listaDeDias			
 
 if __name__ == "__main__":
-	
-	teste = contrato(
-		numeroDeProcesso="E-17/0000/0000/0000",
-		dataAssinatura= datetime.date(2020,3,5),
-		gestor= 1,
-		vencimento= datetime.date(2020,12,12),
-		status= "ATIVO"
-		)
-	
-	vencimentos = teste.gerarVencimentos()
-	print(teste.status)
+	listaDeDados = ['1','006/17',"E-17/0000/0000/0000",'impressora A3','Chada',"2020-12-22","2021-12-22","2021-01-10",'1','ATIVO']
+	contratoTeste = contrato(listaDedadosContrato=listaDeDados)
+	print(contratoTeste.numeroDeProcesso)
+	# contratoTeste.gerarVencimentos()
+
+	vencimentos = contratoTeste.gerarVencimentos()
 	for dia in vencimentos:
 		print(dia)
